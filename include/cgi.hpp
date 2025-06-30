f56bce68f3b7b190c15b2930b3e6e166a7c50bf4
Arnau Cebrián i Ortega <arcebria@student.42barcelona.com>
2025-06-30 21:14:43 +0200

    srry Pol

--- include/cgi.hpp
+++ include/cgi.hpp
@@ -1,21 +1,25 @@
 #include <string>
 #include <cstring>
+#include <iostream>
 #include <wait.h>
 #include <vector>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
 #include "clientRequest.hpp"
-#include "config.hpp"
+#include "../include/config.hpp"
+
 
 class Cgi
 {
 	private:
 		std::string _executable;
-		char** _populateEnv(ClientRequest& clientRequest, Config& config, std::string petition, std::string htmlPath);
-
+		char** _populateEnv(ClientRequest& clientRequest, Config& config, std::string htmlPath);
 	public:
 		Cgi(ClientRequest clientRequest);
 		Cgi();
 		~Cgi();
-		std::string	execScript(std::string htmlPath, std::string petition, ClientRequest& clientRequest, Config& config);
+		std::string	execScript(std::string htmlPath, ClientRequest& clientRequest, Config& config);
 };
 
 std::string getExtension(std::string htmlPath);
